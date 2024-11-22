257ab4f209b33da80d587cb2773aaca4dc82074c
wind-ik <soeag.st@gmail.com>
2024-11-22 23:06:46 +0900

    fix: login page

--- pages/login.vue
+++ pages/login.vue
@@ -23,27 +23,43 @@ const handleSignIn = async () => {
 
 <!-- eslint-disable vue/html-self-closing -->
 <template>
-  <div class="p-10 text-center lg:w-100 backdrop-blur-md rounded-2xl bg-gray-8">
-    <div class="text-3xl font-medium mb-8">Welcome back!</div>
+  <div
+    class="p-10 text-center w-85 lg:w-100 backdrop-blur-md rounded-2xl bg-#0e1111 b-2 b-gray-800 b-solid"
+  >
+    <div class="text-2xl text-gray-100 font-medium mb-1">Welcome!</div>
+    <div class="text-sm text-yellow-500 font-medium mb-6">
+      This is test account, just click sign in :D
+    </div>
     <InputText
-      v-model="email"
+      model-value="test@email.com"
       type="text"
-      class="!py-3 !px-4 !w-full !outline-0 !mb-6 !bg-white/10 !rounded-full"
+      class="!py-2 !px-4 !w-full !outline-0 !mb-4 !bg-gray-800 !text-gray-400 !rounded-full !text-sm"
       placeholder="Email"
+      disabled
     />
-    <InputText
+    <!-- <InputText
       v-model="password"
       type="password"
-      class="!py-3 !px-4 !w-full !outline-0 !mb-6 !bg-white/10 !rounded-full"
-      placeholder="Password"
-    />
-    <button
+      class="!py-2 !px-4 !w-full !outline-0 !mb-4 !bg-gray-900 !text-gray-100 !rounded-full !text-sm"
+      disabled
+    /> -->
+
+    <!-- <button
       type="button"
-      class="max-w-20 w-full text-sm rounded-full py-2 px-2 bg-white/30 hover:bg-white/40 active:bg-white/20 cursor-pointer transition-colors duration-150"
+      class="max-w-20 w-full !text-sm rounded-full py-2 px-2 bg-white/30 hover:bg-white/40 active:bg-white/20 cursor-pointer transition-colors duration-150"
       @click="() => handleSignIn()"
     >
       Sign In
-    </button>
+    </button> -->
+
+    <Button
+      label="Primary"
+      rounded
+      type="button"
+      class="max-w-20 w-full !text-sm rounded-full py-2 px-2 bg-white/30 hover:bg-white/40 active:bg-white/20 cursor-pointer transition-colors duration-150"
+      @click="() => handleSignIn()"
+      >Sign In</Button
+    >
 
     <div v-if="showError === true" class="text-red text-sm mt-3">
       Invalid login credentials!
